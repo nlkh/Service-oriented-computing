a061bc381459a80bf41fae338ee6816640cff2ed
nlkh <nlkh@koreatech.ac.kr>
2020-12-12 03:51:38 +0000

    v0.1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, request
+from flask import Flask, render_template, request, url_for, request, redirect
 import requests
 import json
 import facebook
@@ -11,7 +11,10 @@ kakao_access_token = ""
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    if kakao_access_token == "" :
+        return render_template('superindex.html')
+    else :
+        return render_template('loginindex.html')
 
 @app.route('/oauth')
 def oauth():
@@ -23,7 +26,7 @@ def oauth():
     }
 
     url = "https://kapi.kakao.com/v1/user/signup"
-    headers.update({'Authorization': "Bearer " + str(kakao_access_token)})
+    headers.update({'Authorization': "Bearer " + kakao_access_token})
     response = requests.request("POST", url, headers=headers)
 
     url = "https://kapi.kakao.com/v1/user/me"
@@ -34,7 +37,7 @@ def oauth():
 
     headers = {
         'Content-Type': "application/x-www-form-urlencoded",
-        'Authorization': "Bearer " + str(kakao_access_token),
+        'Authorization': "Bearer " + kakao_access_token,
     }
 
     response = requests.get(url, headers=headers)
@@ -42,21 +45,20 @@ def oauth():
     url = "https://kapi.kakao.com/v1/api/talk/friends"  ##친구 목록 불러오기
     response = requests.get(url, headers=headers)
 
-    return response.json()
+    return redirect("https://serviceoriented.ml:5000/")
 
 
 def token():
     global kakao_access_token
     code = str(request.args.get('code'))
     url = "https://kauth.kakao.com/oauth/token"
-    payload = "grant_type=authorization_code&client_id=43e3daf58dd14d049001ebdbd6538f58&redirect_uri=https://serviceoriented.ml:5000/oauth&code=" + str(
-        code)
+    payload = "grant_type=authorization_code&client_id=43e3daf58dd14d049001ebdbd6538f58&redirect_uri=https://serviceoriented.ml:5000/oauth&code=" + str(code)
     headers = {
         'Content-Type': "application/x-www-form-urlencoded",
         'Cache-Control': "no-cache",
     }
     response = requests.request("POST", url, data=payload, headers=headers)
-    kakao_access_token = json.loads(((response.text).encode('utf-8')))['access_token']
+    kakao_access_token = str(json.loads(((response.text).encode('utf-8')))['access_token'])
 
     return kakao_access_token
 
@@ -66,23 +68,9 @@ def kakaostory():
     global kakao_access_token
     #access_token = token()
     print(kakao_access_token)
-
-    # 게시물 쓰기
-    url = "https://kapi.kakao.com/v1/api/story/post/note"
-    payload = 'content=POSTING_TEST'  # <-내용
-    headers = {
-        'Content-Type': "application/x-www-form-urlencoded",
-        'Cache-Control': "no-cache",
-        "Authorization": f"Bearer {kakao_access_token}"
-    }
-    requests.request("POST", url, data=payload, headers=headers)
-
-    requests.get("https://kapi.kakao.com/v1/api/story/profile",
-                 headers={"Authorization": f"Bearer {kakao_access_token}"}, )
-    # id값 스토리 받기
-    response = requests.get("https://kapi.kakao.com/v1/api/story/mystory?id=_CCc7Q8.hCxTbltQ9uA",
-                            headers={"Authorization": f"Bearer {kakao_access_token}"}, )
-    return (response.text)
+    response = requests.get("https://kapi.kakao.com/v1/api/story/mystories", 
+                            headers={"Authorization":f"Bearer {kakao_access_token}"})
+    return response.text
 
 @app.route('/friend_message')
 def friend_message():
@@ -115,26 +103,26 @@ def friend_message():
         print(response.status_code)
         return response.text
 
-
 @app.route('/logout')
 def logout():
+    global kakao_access_token
     url = "https://kapi.kakao.com/v1/user/logout"
     headers = {
-        "Authorization": "Bearer " + str(token())
+        "Authorization" : "Bearer " + kakao_access_token
     }
-    response = requests.post(url, headers=headers)
-    return response.json()
+    response = requests.request("POST", url, headers=headers)
+    kakao_access_token=""
+    return redirect("https://serviceoriented.ml:5000/")
 
 @app.route('/fb-login')
 def fb_login():
     return render_template('fbtest.html')
-
     
 @app.route('/fb-login/oauth')
 def fb_oauth():
     global fb_access_token
     code = str(request.args.get('code'))
-    url = 'https://graph.facebook.com/v9.0/oauth/access_token?client_id=1226404121075869&redirect_uri=https://serviceoriented.ml:5000/fb-login/oauth&client_secret=ae1b40de9370d3ef5290ae4cb9f4191b&code=' + str(code)
+    url = 'https://graph.facebook.com/v9.0/oauth/access_token?client_id=822581321822671&redirect_uri=https://serviceoriented.ml:5000/fb-login/oauth&client_secret=2b2581f4b49f0bc6b7051ea5de33051c&code=' + str(code)
 
     response = requests.request("GET", url)
     fb_access_token = str(json.loads(response.text)['access_token'])
--- fbposts.py
+++ fbposts.py
@@ -1,6 +1,6 @@
 import facebook
 
 def load_posts() :
-    graph = facebook.GraphAPI(access_token="EAALsIiN8cc8BAMKnKDr4s21Ng1jFbVSYTjN7IJFq2EKmHO4OqCtMjiBf78yH1AvaEHMzOiAXVR0twOnYVJXtGD2pOitXL8F5eX2ZCEVu8jbsjPuyZCydnRl4xGA7ly74NAF0JZCgBi1YRkig18o6sD5Nee1awnXMcqjEo7RQArpjlkhaDiPnYPVXhkJ7tjTc57EInmkDVdeNDZCJ3Y6uhz6bLjgnXFXm3qsmUOWnxQZDZD", version="3.1")
+    graph = facebook.GraphAPI(access_token="EAALsIiN8cc8BAMKnKDr4s21Ng1jFbVSYTjN7IJFq2EKmHO4OqCtMjiBf78yH1AvaEHMzOiAXVR0twOnYVJXtGD2pOitXL8F5eX2ZCEVu8jbsjPuyZCydnRl4xGA7ly74NAF0JZCgBi1YRkig18o6sD5Nee1awnXMcqjEo7RQArpjlkhaDiPnYPVXhkJ7tjTc57EInmkDVdeNDZCJ3Y6uhz6bLjgnXFXm3qsmUOWnxQZDZD", version="9.0")
     post = graph.get_object(id='me', fields='posts')
     return post['posts']['data']
